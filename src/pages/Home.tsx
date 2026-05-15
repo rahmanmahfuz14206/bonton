a33b4faafca4feb37a22e83af65f5967bf726494
rahmanmahfuz14206 <144756829+rahmanmahfuz14206@users.noreply.github.com>
2026-05-16 03:09:54 +0600

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
 import { Share2, Users, FileUp, Shield } from "lucide-react";
-import icon from "../../logo.png";
 
 export default function Home() {
   const [roomId, setRoomId] = useState("");
@@ -26,7 +25,7 @@ export default function Home() {
       >
         <div className="mb-6 flex justify-center">
           <div className="overflow-hidden rounded-2xl bg-white p-1 shadow-md border-2 border-slate-900">
-            <img src=icon alt="Bonton Logo" className="h-16 w-16 md:h-20 md:w-20 object-contain" />
+            <img src="/logo.png" alt="Bonton Logo" className="h-16 w-16 md:h-20 md:w-20 object-contain" />
           </div>
         </div>
         
