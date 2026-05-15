c13eb06d23847f4fadf7c5bd7f66254407ffb2fb
rahmanmahfuz14206 <144756829+rahmanmahfuz14206@users.noreply.github.com>
2026-05-16 03:10:39 +0600

    Update Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,7 +3,6 @@ import { useParams, useSearchParams, useNavigate } from "react-router-dom";
 import { io, Socket } from "socket.io-client";
 import { QRCodeSVG } from "qrcode.react";
 import { motion, AnimatePresence } from "motion/react";
-import logo from "../../logo.png";
 import { 
   Plus,
   File, 
@@ -110,7 +109,7 @@ export default function Room() {
       if (!receivedChunks.current[id]) return;
 
       const chunks = receivedChunks.current[id];
-      const totalSize = chunks.reduce((acc: number, curr: ArrayBuffer) => acc + curr.byteLength, 0);
+      const totalSize = chunks.reduce((acc, curr) => acc + curr.byteLength, 0);
       const combinedBuffer = new Uint8Array(totalSize);
       let offset = 0;
       for (const chunk of chunks) {
@@ -269,7 +268,7 @@ export default function Room() {
           <div 
             className="overflow-hidden flex h-10 w-10 md:h-14 md:w-14 items-center justify-center rounded-xl md:rounded-2xl bg-white shadow-md border-2 border-slate-900 p-1"
           >
-            <img src=logo alt="Bonton Logo" className="h-full w-full object-contain" />
+            <img src="/logo.png" alt="Bonton Logo" className="h-full w-full object-contain" />
           </div>
           <div>
             <h1 className="text-3xl md:text-5xl font-brand text-slate-900 leading-tight">
