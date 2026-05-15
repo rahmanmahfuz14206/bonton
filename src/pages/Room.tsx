5e60c8f51474a8ff8650c9c594580df49c9693dc
rahmanmahfuz14206 <144756829+rahmanmahfuz14206@users.noreply.github.com>
2026-05-16 02:36:06 +0600

    Update Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -269,7 +269,7 @@ export default function Room() {
           <div 
             className="overflow-hidden flex h-10 w-10 md:h-14 md:w-14 items-center justify-center rounded-xl md:rounded-2xl bg-white shadow-md border-2 border-slate-900 p-1"
           >
-            <img src="logo" alt="Bonton Logo" className="h-full w-full object-contain" />
+            <img src=logo alt="Bonton Logo" className="h-full w-full object-contain" />
           </div>
           <div>
             <h1 className="text-3xl md:text-5xl font-brand text-slate-900 leading-tight">
